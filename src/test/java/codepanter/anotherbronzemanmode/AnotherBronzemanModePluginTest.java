ee7ff4a0852e2be3ae2ab701682040b490a65196
Nick Denaro <ndenaro@osisoft.com>
2021-05-15 14:42:49 -0400

    Why are line endings still a problem in 2021?

--- src/test/java/codepanter/anotherbronzemanmode/AnotherBronzemanModePluginTest.java
+++ src/test/java/codepanter/anotherbronzemanmode/AnotherBronzemanModePluginTest.java
@@ -1,1 +1,13 @@
-package codepanter.anotherbronzemanmode;import net.runelite.client.RuneLite;import net.runelite.client.externalplugins.ExternalPluginManager;public class AnotherBronzemanModePluginTest{    public static void main(String[] args) throws Exception    {        ExternalPluginManager.loadBuiltin(AnotherBronzemanModePlugin.class);        RuneLite.main(args);    }}
+package codepanter.anotherbronzemanmode;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class AnotherBronzemanModePluginTest
+{
+    public static void main(String[] args) throws Exception
+    {
+        ExternalPluginManager.loadBuiltin(AnotherBronzemanModePlugin.class);
+        RuneLite.main(args);
+    }
+}
