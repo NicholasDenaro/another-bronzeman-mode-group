0c9bc79dd5133523dc5010a557042ca045be8948
Nick Denaro <ndenaro@osisoft.com>
2021-05-15 14:40:56 -0400

    Change line endings

--- src/test/java/codepanter/anotherbronzemanmode/AnotherBronzemanModePluginTest.java
+++ src/test/java/codepanter/anotherbronzemanmode/AnotherBronzemanModePluginTest.java
@@ -1,13 +1,1 @@
-package codepanter.anotherbronzemanmode;
-
-import net.runelite.client.RuneLite;
-import net.runelite.client.externalplugins.ExternalPluginManager;
-
-public class AnotherBronzemanModePluginTest
-{
-    public static void main(String[] args) throws Exception
-    {
-        ExternalPluginManager.loadBuiltin(AnotherBronzemanModePlugin.class);
-        RuneLite.main(args);
-    }
-}
+package codepanter.anotherbronzemanmode;import net.runelite.client.RuneLite;import net.runelite.client.externalplugins.ExternalPluginManager;public class AnotherBronzemanModePluginTest{    public static void main(String[] args) throws Exception    {        ExternalPluginManager.loadBuiltin(AnotherBronzemanModePlugin.class);        RuneLite.main(args);    }}
